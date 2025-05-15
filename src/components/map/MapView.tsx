43988b0d31a07c099a7e8de36a97562e7b2d298f
Jimmy Briggs <jimmy.briggs@noclocks.dev>
2025-05-15 15:41:17 -0400

    Add default export to MapView component

--- src/components/map/MapView.tsx
+++ src/components/map/MapView.tsx
@@ -1,1 +1,54 @@
-// Content of MapView.tsx as shown in the file changes above
+import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+
+// Fix Leaflet default marker icon issue
+delete (L.Icon.Default.prototype as any)._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
+  iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+});
+
+interface MapViewProps {
+  center: [number, number];
+  zoom: number;
+  properties?: Array<{
+    id: string;
+    latitude: number;
+    longitude: number;
+    address: string;
+    price: number;
+  }>;
+}
+
+const MapView: React.FC<MapViewProps> = ({ center, zoom, properties = [] }) => {
+  return (
+    <MapContainer
+      center={center}
+      zoom={zoom}
+      style={{ height: '100%', width: '100%' }}
+    >
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      />
+      {properties.map((property) => (
+        <Marker
+          key={property.id}
+          position={[property.latitude, property.longitude]}
+        >
+          <Popup>
+            <div>
+              <p>{property.address}</p>
+              <p>${property.price.toLocaleString()}</p>
+            </div>
+          </Popup>
+        </Marker>
+      ))}
+    </MapContainer>
+  );
+};
+
+export default MapView;
